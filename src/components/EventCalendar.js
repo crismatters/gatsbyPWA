04fe71cce7a4a540aaf19e537718f632518e899e
GeorgeSteel <julianacero98@gmail.com>
2020-03-21 15:21:46 -0600

    Libreria react-loadable añadida para cargar de manera dinamica fullcalendar. Comando build funcionando.

--- src/components/EventCalendar.js
+++ src/components/EventCalendar.js
@@ -1,23 +1,18 @@
-import React,{Component} from 'react';
-import '@fullcalendar/daygrid/main.css';
-import '@fullcalendar/core/main.css';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from "react"
+import "@fullcalendar/daygrid/main.css"
+import "@fullcalendar/core/main.css"
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import Grid from "@material-ui/core/Grid"
 
-
-export default class EventCalendar extends Component{
-  render(){
-    return(
+export default class EventCalendar extends Component {
+  render() {
+    return (
       <Grid container spacing={3}>
-        <Grid item xs={1}/>
-        <Grid item xs={10}>
-            <FullCalendar
-              defaultView="dayGridWeek" plugins={[ dayGridPlugin ]}
-            />
+        <Grid item xs={12}>
+          <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
         </Grid>
-        <Grid item xs={1}/>
       </Grid>
-    );
+    )
   }
 }
